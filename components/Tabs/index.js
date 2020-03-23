e9f12274b130e46a0f68d00813c1d2a3c37927da
Mark Rivera <Mriver1991@gmail.com>
2020-03-22 21:37:55 -0400

    finished sprint

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,22 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+
+axios.get('https://lambda-times-backend.herokuapp.com/topics').then( response => {
+    let info = response.data.topics.map(element => {
+        let node = document.createElement('div');
+        node.classList.add('tab');
+        node.textContent = element;
+
+        return node;
+    })
+
+    let topic = document.querySelector('.topics');
+
+    info.forEach(element => topic.appendChild(element));
+}).catch(err => {
+    let topic = document.querySelector('.topics');
+    topic.textContent = "Sorry, an error has occurred!";
+})
+
